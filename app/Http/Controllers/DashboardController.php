6c5db698ec2f2f9e6f041f440448e169b857591a
Moritz Graf <moritzgraf@MacBook-Pro-von-Moritz.local>
2023-12-30 15:37:48 +0100

    reworked the layout of the dashboard, made the calendar clickable

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -11,17 +11,33 @@ use App\Models\Module;
 use App\Models\Task;
 use App\Models\Event;
 use Illuminate\Http\Request;
+use App\Enums\ModuleStatus;
 
 class DashboardController extends Controller {
-    public function index() {
-        $user_id = User::query()->where('id', '=', 1)->get()[0]->id;
-        $modules = Module::query()->where('user_id', '=', $user_id)->get();
-        $tasks = Task::query()->where('user_id', '=', $user_id)->get();
+    public function index(Request $request) {
+        $user_id = $request
+            ->user()->id;
+        $modules = Module::query()
+            ->where('user_id', '=', $user_id)
+            ->get();
+        $tasks = Task::query()
+            ->where('user_id', '=', $user_id)
+            ->get();
         $events = Event::query()
             ->where('user_id', $user_id)
-            ->whereDate('start', '>=', now()->toDateString())
-            ->whereDate('start', '<=', now()->addDay()->toDateString())
+            ->where(function ($query) {
+                // Events starting within the next seven days
+                $query->whereDate('start', '>=', now()->toDateString())
+                    ->whereDate('start', '<=', now()->addDays(7)->toDateString());
+
+                // OR Events that started in the past and will end within the next seven days
+                $query->orWhere(function ($query) {
+                    $query->whereDate('end', '>=', now()->toDateString())
+                        ->whereDate('end', '<=', now()->addDays(7)->toDateString());
+                });
+            })
             ->get();
+
 
         // Get all module ID's in one variable;
         // Also count the amount of modules done
@@ -29,8 +45,9 @@ class DashboardController extends Controller {
         $modules_done = 0;
         foreach($modules as $module){
             $module_ids[] = $module->id;
-
-            if ($module->status >= 100){
+            if ($module->status == ModuleStatus::WaitingForResult or
+                $module->status == ModuleStatus::DoneWithGrade or
+                $module->status == ModuleStatus::DoneWithoutGrade ){
                 $modules_done++;
             }
         }
@@ -40,19 +57,22 @@ class DashboardController extends Controller {
 
         // Sum the amount of credit points over all exams and count those that are done
         // An exam is considered done if it has been graded with a passing grade <= 4
-        // Also calculate the average grade over all exams
+        // Also calculate the average grade over all exams, weighted by credit points
         $credit_points_total = 0;
         $credit_points_achieved = 0;
         $grade_sum = 0;
         foreach($exams as $exam) {
-            $credit_points_total += $exam->credit_points;
-            $grade_sum += $exam->grade;
-
             if ($exam->grade <= 4) {
                 $credit_points_achieved += $exam->credit_points;
+                $credit_points_total += $exam->credit_points;
+                $grade_sum += $exam->grade * $exam->credit_points;
             }
         }
-        $grade_average = $grade_sum / count($exams);
+
+        $grade_average = 0;
+        if (count($exams) != 0){
+            $grade_average = $grade_sum / count($exams);
+        }
 
         return Inertia::render('Dashboard',
             ['credit_points_total' => $credit_points_total,
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -12,100 +12,120 @@ defineProps({ credit_points_total: Number,
                     events: Array
 })
 </script>
-
 <template>
-	<Head title="Übersicht" />
+<Head title="Übersicht" />
 
-	<AuthenticatedLayout>
+
+    <AuthenticatedLayout>
 		<template #header>
 			<h1 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Dashboard</h1>
 		</template>
 
 		<!-- 0 - This is the outermost Div -->
-		<div class="py-12">
+		<div class="py-6">
 
 			<!-- 1 - This is the second outermost Div-->
 			<div class="max-w-7x1 mx-auto sm:px-6 lg:px-8 space-y-6">
 
-				<!-- 2 - This is the top Div for Progress, Credit Points and Average Grade -->
-				<div class="p-4 sm:p-8 bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg flex gap-4 content-between justify-center">
-
-					<!-- 2l - This is the Div for the Progress Bar-->
-					<div class="p-4 sm:p-8 bg-green-800 dark:bg-green-800 overflow-hidden shadow-sm sm:rounded-lg w-1/3">
-                        <h1 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                        </h1>
-                        <div class="progress-bar">
-                            <div class="progress-bar-fill" :style="{ width: completionPercentage + '%' }"></div>
-                            <div class="progress-text">{{ modules_done }} von {{ modules_total }} Modulen</div>
-                        </div>
-					</div>
-
-					<!-- 2c - This is the Div for the Credit Points Bar-->
-                    <div class="p-4 sm:p-8 bg-green-800 dark:bg-green-800 overflow-hidden shadow-sm sm:rounded-lg w-1/3 flex items-center justify-center">
-                        <h1 class="font-semibold text-3xl text-gray-800 dark:text-gray-200 leading-tight">
-                            Du hast {{ credit_points_achieved }} von {{ credit_points_total }} CP erreicht.
-                        </h1>
-                    </div>
-
-					<!-- 2r - This is the Div for the Average Grade Bar-->
-                    <div class="p-4 sm:p-8 bg-green-800 dark:bg-green-800 overflow-hidden shadow-sm sm:rounded-lg w-1/3 flex items-center justify-center">
-                        <h1 class="font-semibold text-3xl text-gray-800 dark:text-gray-200 leading-tight">
-                            Deine Durschnittsnote: {{ grade_average }}
-                        </h1>
-                    </div>
-				</div>
-
 				<!-- 3 - This is the bottom Div for ToDo's and Calendar-->
-				<div class="p-4 sm:p-8 bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg flex gap-4 content-between justify-center">
+				<div class="p-2 sm:p-4 bg-opacity-0 overflow-hidden shadow-sm sm:rounded-lg flex gap-4 content-between justify-center">
 					<!-- 3l - This is the bottom left Div for the ToDo title and the ToDo items-->
-					<div class="p-4 sm:p-8 bg-gray-100 dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg w-3/4">
+					<div class="p-2 sm:p-4 bg-gray-100 dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg w-3/4">
 						<!-- 3lt - This is the bottom left top Div for ToDo title-->
-						<div class="p-4 sm:p-8 bg-red-800 dark:bg-red-800 overflow-hidden shadow-sm sm:rounded-lg">
-                            <h1 class="flex justify-center items-center font-semibold text-3xl text-gray-800 dark:text-gray-200 leading-tight">
-                                Deine ToDo's:
+						<div class="bg-opacity-0">
+                            <h1 class="flex justify-center font-semibold text-3xl text-gray-800 dark:text-gray-200 leading-tight">
+                                Deine Module
                             </h1>
+                            <div class="border-t border-gray-500 my-2"></div>
 						</div>
 
-						<!-- 3lb - This is the bottom left bottom Div for ToDo items-->
+						<!-- 3lb - This is the bottom left bottom Div for Modules and associated Tasks-->
 						<div class="p-4 flex gap-4 content-between justify-center">
 
 						<!-- 3lbl - This is the bottom left bottom left Div for the first ToDo item-->
-                            <div v-if="tasks[0] != null" class="p-4 sm:p-8 bg-green-200 dark:bg-green-800 overflow-hidden shadow-sm sm:rounded-lg w-1/3">
+                            <div v-if="tasks[0] != null" class="p-4 sm:p-8 bg-gray-200 overflow-hidden shadow-sm sm:rounded-lg w-1/3">
                                 {{ tasks[0].title }}<br>
-                                <hr>
+                                <div class="border-t border-gray-500 my-2"></div>
                                 {{ tasks[0].description }}
                             </div>
-                            <div v-else class="p-4 sm:p-8 bg-green-200 dark:bg-green-800 overflow-hidden shadow-sm sm:rounded-lg w-1/3">
+                            <div v-else class="p-4 sm:p-8 bg-gray-200 shadow-sm sm:rounded-lg w-1/3">
                                 Du hast keine offenen Tasks - Yay!
                             </div>
 
 							<!-- 3lbl - This is the bottom left bottom center Div for the second ToDo item-->
-                            <div v-if="tasks[1] != null" class="p-4 sm:p-8 bg-green-200 dark:bg-green-800 overflow-hidden shadow-sm sm:rounded-lg w-1/3">
+                            <div v-if="tasks[1] != null" class="p-4 sm:p-8 bg-gray-200 overflow-hidden shadow-sm sm:rounded-lg w-1/3">
                                 {{ tasks[1].title }}<br>
-                                <hr>
+                                <div class="border-t border-gray-500 my-2"></div>
                                 {{ tasks[1].description }}
                             </div>
-                            <div v-else class="p-4 sm:p-8 bg-green-200 dark:bg-green-800 overflow-hidden shadow-sm sm:rounded-lg w-1/3">
+                            <div v-else class="p-4 sm:p-8 bg-gray-200 overflow-hidden shadow-sm sm:rounded-lg w-1/3">
                                 Kein weiterer Task zum Anzeigen - Yay!
                             </div>
 
 							<!-- 3blr - This is the bottom left bottom right Div for the third Todo item-->
-                            <div v-if="tasks[2] != null" class="p-4 sm:p-8 bg-green-200 dark:bg-green-800 overflow-hidden shadow-sm sm:rounded-lg w-1/3">
+                            <div v-if="tasks[2] != null" class="p-4 sm:p-8 bg-gray-200 overflow-hidden shadow-sm sm:rounded-lg w-1/3">
                                 {{ tasks[2].title }}<br>
-                                <hr>
+                                <div class="border-t border-gray-500 my-2"></div>
                                 {{ tasks[2].description }}
                             </div>
-                            <div v-else class="p-4 sm:p-8 bg-green-200 dark:bg-green-800 overflow-hidden shadow-sm sm:rounded-lg w-1/3">
+                            <div v-else class="p-4 sm:p-8 bg-gray-200 overflow-hidden shadow-sm sm:rounded-lg w-1/3">
                                 Kein weiterer Task zum Anzeigen - Yay!
                             </div>
-
 						</div>
 					</div>
 
 					<!-- 3r - This is the bottom left Div for the Calendar-->
-					<div class="p-4 sm:p-8 bg-gray-100 dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg w-1/4">
-                        <h2>Deine Events:</h2>
-                        <div class="scrollable-container">
+					<div class="p-2 sm:p-4 bg-gray-100 dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg w-1/4">
+                        <div class="flex items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18 9 11.25l4.306 4.306a11.95 11.95 0 0 1 5.814-5.518l2.74-1.22m0 0-5.94-2.281m5.94 2.28-2.28 5.941" />
+                            </svg>
+                            <h2 class="text-xl font-bold underline ml-2">Deine Stats</h2>
+                        </div>
+
+                        <!-- 2 - This is the top Div for Progress, Credit Points, and Average Grade -->
+                        <div class="bg-opacity-0 overflow-hidden shadow-sm sm:rounded-lg flex-col">
+
+                            <!-- 2l - This is the Div for the Progress Bar-->
+                            <div class="p-2 sm:p-4 bg-opacity-0">
+                                <div class="progress-bar flex">
+                                    <div class="progress-bar-fill" :style="{ width: completionPercentage + '%' }"></div>
+                                    <div class="progress-text">{{ modules_done }} von {{ modules_total }} Modulen</div>
+                                </div>
+                            </div>
+
+
+
+                            <!-- 2c - This is the Div for the Credit Points -->
+                            <div class="p-2 sm:p-4 bg-opacity-0 flex items-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" class="w-8 h-8">
+                                        <path strokeLinecap="round" strokeLinejoin="round" d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5" />
+                                    </svg>
+                                    <h1 class="font-semibold text-xl ml-2">
+                                        {{ credit_points_achieved }} / {{ credit_points_total }} Credit Points
+                                    </h1>
+                            </div>
+
+                            <!-- 2r - This is the Div for the Average Grade -->
+                            <div class="p-2 sm:p-4 bg-opacity-0 flex items-center">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0 0 12 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75Z" />
+                                </svg>
+
+                                <h1 class="font-semibold text-xl ml-2">
+                                    {{ grade_average }} Durschnittsnote
+                                </h1>
+                            </div>
+                        </div>
+
+                        <div class="flex items-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z" />
+                            </svg>
+                            <h2 class="text-xl font-bold underline ml-2">Deine Events</h2>
+                        </div>
+
+                        <div class="scrollable-container" @click="redirectToEvent">
                             <div class="event" v-for="(event, index) in events" :key="index">
                                 <div class="event-date">{{ event.date }}</div>
                                 <div class="event-details">
@@ -128,7 +148,6 @@ defineProps({ credit_points_total: Number,
 		components: {
 			Qalendar,
 		},
-
 		methods: {
 			showEventModal(props){
 				this.event = props.clickedEvent;
@@ -137,8 +156,10 @@ defineProps({ credit_points_total: Number,
 			},
 			hideEventModal(){
 				this.eventDialogVisible = false;
-
-			}
+			},
+            redirectToEvent(eventId) {
+                window.location.href = `http://wbh.test/calendar`;
+            },
 		},
 
 		computed: {
@@ -148,8 +169,9 @@ defineProps({ credit_points_total: Number,
 					return event;
 				})
 			},
+            // Always return a little bit so that the bar is not invisible at 0 % progress
             completionPercentage(){
-                return (this.modules_done / (this.modules_total)) * 100;
+                return 1 + (this.modules_done / (this.modules_total)) * 99;
             }
 		},
         data() {
@@ -170,10 +192,10 @@ defineProps({ credit_points_total: Number,
 @import "qalendar/dist/style.css";
 
 .progress-bar {
-    height: 75px;
-    border: 1px solid #ccc;
+    height: 33px;
+    border: 0px solid #ccc;
     position: relative;
-    border-radius: 5px;
+    border-radius: 0px;
 }
 
 .progress-bar-fill {
