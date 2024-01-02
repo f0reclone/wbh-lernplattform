fe4b0c2348df7bdd7619a2e81a9162f3e4c59da5
Florian Hellmann <florian.hellmann@posteo.de>
2024-01-02 19:36:50 +0100

    BoardControler fix

--- app/Http/Controllers/BoardController.php
+++ app/Http/Controllers/BoardController.php
@@ -7,21 +7,20 @@ namespace App\Http\Controllers;
 use App\Http\Resources\ModuleResource;
 use App\Http\Resources\TaskResource;
 use App\Models\Module;
+use Illuminate\Http\Request;
 use Inertia\Inertia;
 use App\Models\User;
 use App\Models\Task;
 
 class BoardController extends Controller
 {
-    public function index() {
-        $user_id = User::query()
-            ->where('id', '=', 1)
-            ->get()[0]->id;
+    public function index(Request $request) {
+        $user = $request->user();
         $tasks = Task::query()
-            ->where('user_id', '=', $user_id)
+            ->where('user_id', '=', $user->id)
             ->get();
         $modules = Module::query()
-            ->where('user_id', '=', $user_id)
+            ->where('user_id', '=', $user->id)
             ->get();
 
         return Inertia::render('Board', [
